cb2390d018475e1ce38a148878ecee6d55f39114
Tran Minh Tuan <tuantranf@gmail.com>
2019-01-10 10:31:39 +0700

    add Pypi package deploy setting

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='kubeb',
-    version='0.0.1',
-    packages=['kubeb'],
+    version='0.0.2',
+    author="podder-ai",
+    description="A small tool to deploy app to Kubernetes",
+    packages=setuptools.find_packages(),
     include_package_data=True,
+    long_description_content_type="text/markdown",
+    url="https://github.com/podderai/kubeb",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'click',
+        'jinja2',
+        'pyyaml',
+        'python-dotenv',
+        'click-spinner',
     ],
-    entry_points='''
-        [console_scripts]
-        kubeb=cli:cli
-    ''',
+    entry_points={
+        'console_scripts': ['kubeb=cli:cli'],
+    }
 )
